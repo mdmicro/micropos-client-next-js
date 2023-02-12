38384ea1142238f666e4e59f2574ad784961afda
mdmicro <etomymail@gmail.com>
2023-02-13 01:16:15 +0500

    example integration with NESTjs when 'companies' table

--- src/pages/api/companie.ts
+++ src/pages/api/companie.ts
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import axios from "axios";
 
 type Data = {
   name: string
@@ -13,5 +14,11 @@ export default function handler(
 ) {
   console.log('REQUEST COMPANIE!!!')
   console.log(req.body)
-  res.status(200).send(req.body)
+  const url = 'http://localhost:3000/companies'
+  axios.post(url, req.body).then(response=>{
+    console.log('Post companie complete!')
+    console.log(response.status)
+    console.log(response.data)
+    res.status(response.status).send(response.data)
+  }).catch(e=>console.log(e))
 }
